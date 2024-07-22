e7833e0716ba434b68f2803de246673398077722
Denis Emelyanov <10035052+DenisEmelyanov@users.noreply.github.com>
2024-07-22 17:08:03 -0400

    Update transaction-form.component.ts

--- src/app/component/transaction-form/transaction-form.component.ts
+++ src/app/component/transaction-form/transaction-form.component.ts
@@ -12,51 +12,54 @@ import { Transaction } from 'src/app/model/transaction';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 
-export class TransactionFormComponent implements OnInit{
+export class TransactionFormComponent implements OnInit {
   inputData: any;
   editData: any;
-  transactionForm : any;
+  transactionForm: any;
   selectedType: any;
 
   constructor(@Inject(MAT_DIALOG_DATA) public data: any, private ref: MatDialogRef<PopupComponent>, private formBuilder: FormBuilder) {
-
-      this.transactionForm = this.formBuilder.group({
-        id: this.formBuilder.control(this.data.transaction.id),
-        ticker: this.formBuilder.control(this.data.ticker),
-        side: this.formBuilder.control(''),
-        quantity: this.formBuilder.control(1),
-        type: this.formBuilder.control(''),
-        strike: this.formBuilder.control(''),
-        expiration: this.formBuilder.control(null),
-        premium: this.formBuilder.control(''),
-        openDate: this.formBuilder.control(null),
-        closeDate: this.formBuilder.control(null)
-      });
+    this.transactionForm = this.formBuilder.group({
+      id: this.formBuilder.control(this.data.transaction.id),
+      ticker: this.formBuilder.control(this.data.ticker),
+      side: this.formBuilder.control(''),
+      quantity: this.formBuilder.control(1),
+      type: this.formBuilder.control(''),
+      strike: this.formBuilder.control(''),
+      expiration: this.formBuilder.control(null),
+      premium: this.formBuilder.control(''),
+      openDate: this.formBuilder.control(null),
+      closeDate: this.formBuilder.control(null)
+    });
   }
 
   ngOnInit(): void {
     this.inputData = this.data;
+    
     console.warn(this.inputData.id);
-    if (this.inputData.title === "Edit"){
+    if (this.inputData.title === "Edit") {
       this.setFormData(this.inputData.transaction)
     }
   }
 
   setFormData(transaction: Transaction) {
-      this.editData = transaction;
-      console.warn('set form data is called');
-      console.warn(transaction.id);
-      this.transactionForm.patchValue({
-        //ticker: this.editData.ticker, 
-        side: this.editData.side, 
-        quantity: this.editData.quantity,
-        type: this.editData.type, 
-        strike: this.editData.strike,
-        expiration: this.editData.expiration,
-        premium: this.editData.premium,
-        openDate: this.editData.openDate,
-        closeDate: this.editData.closeDate
-      })
+    this.editData = transaction;
+    console.warn('set form data is called');
+    console.warn(transaction.id);
+    console.warn(transaction.type);
+    //set transaction type
+    this.selectedType = this.editData.type;
+    this.transactionForm.patchValue({
+      //ticker: this.editData.ticker, 
+      side: this.editData.side,
+      quantity: this.editData.quantity,
+      type: this.editData.type,
+      strike: this.editData.strike,
+      expiration: this.editData.expiration,
+      premium: this.editData.premium,
+      openDate: this.editData.openDate,
+      closeDate: this.editData.closeDate
+    })
   }
 
   saveTransaction() {
