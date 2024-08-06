89a74616f3c7e47c25e5d8ba68105996ae294601
Denis Emelyanov <10035052+DenisEmelyanov@users.noreply.github.com>
2024-08-05 22:40:49 -0400

    added grid to dialog

--- src/app/component/trades-details-dialog/trades-details-dialog.component.ts
+++ src/app/component/trades-details-dialog/trades-details-dialog.component.ts
@@ -1,6 +1,8 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, ViewChild } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { PopupComponent } from '../popup/popup.component';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 
 @Component({
   selector: 'app-trades-details-dialog',
@@ -9,10 +11,23 @@ import { PopupComponent } from '../popup/popup.component';
 })
 export class TradesDetailsDialogComponent {
 
+  dataSource: any;
+
+  displayedColumns: string[] = ["transaction", "chips", "openDate", "closeDate", "premium"];
+  @ViewChild(MatPaginator) paginatior !: MatPaginator;
+  @ViewChild(MatSort) sort !: MatSort;
+
   constructor(@Inject(MAT_DIALOG_DATA) public data: any, private ref: MatDialogRef<PopupComponent>) {
     
   }
   closeDialog() {
     this.ref.close();
+  }
+
+  public getColor(value: number): string {
+    if (value === 0)
+      return "black";
+    else
+      return value > 0 ? "green" : "red";
   }
 }
